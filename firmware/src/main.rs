f325db3490f5fdb15149c2d10f8b93a1c69c581e
Pegasis <me@pegasis.site>
2025-05-02 16:00:30 -0400

    ozys testing

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -3,21 +3,37 @@
 
 use {defmt_rtt as _, panic_probe as _};
 
+use defmt::info;
 use embassy_executor::Spawner;
 use embassy_stm32::gpio::{Level, Output, Speed};
 use embassy_time::{Duration, Timer};
-use defmt::info;
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
-    let mut led = Output::new(p.PB7, Level::High, Speed::Low);
+    let mut status_led = Output::new(p.PB14, Level::Low, Speed::Low);
+    let mut led1 = Output::new(p.PC13, Level::Low, Speed::Low);
+    let mut led2 = Output::new(p.PA10, Level::Low, Speed::Low);
+    let mut led3 = Output::new(p.PB6, Level::Low, Speed::Low);
+    let mut led4 = Output::new(p.PB7, Level::Low, Speed::Low);
+
+    info!("Hello OZYS V3!");
 
     loop {
-        info!("Hello, World!");
-        led.set_high();
+        status_led.set_high();
         Timer::after(Duration::from_millis(500)).await;
-        led.set_low();
+        status_led.set_low();
+        led1.set_high();
         Timer::after(Duration::from_millis(500)).await;
+        led1.set_low();
+        led4.set_high();
+        Timer::after(Duration::from_millis(500)).await;
+        led4.set_low();
+        led3.set_high();
+        Timer::after(Duration::from_millis(500)).await;
+        led3.set_low();
+        led2.set_high();
+        Timer::after(Duration::from_millis(500)).await;
+        led2.set_low();
     }
 }
