12899b00cc83b4fe8a192d1a23f7f416550e896f
Pegasis <me@pegasis.site>
2025-05-02 17:39:43 -0400

    clock config

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -5,12 +5,56 @@ use {defmt_rtt as _, panic_probe as _};
 
 use defmt::info;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_stm32::{
+    gpio::{Level, Output, Speed},
+    time::mhz,
+};
 use embassy_time::{Duration, Timer};
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    let p = embassy_stm32::init(Default::default());
+    let config = {
+        use embassy_stm32::rcc::mux::*;
+        use embassy_stm32::rcc::*;
+        let mut config = embassy_stm32::Config::default();
+        let rcc = &mut config.rcc;
+
+        rcc.hsi = false;
+        rcc.hse = Some(Hse {
+            freq: mhz(16),
+            mode: HseMode::Oscillator,
+        });
+        rcc.hsi48 = Some(Hsi48Config {
+            sync_from_usb: true,
+        });
+
+        // Limit clock speed to 150Mhz to reduce power consumption
+        rcc.pll = Some(Pll {
+            source: PllSource::HSE,
+            prediv: PllPreDiv::DIV4,
+            mul: PllMul::MUL75,
+            divp: Some(PllPDiv::DIV2),
+            divq: Some(PllQDiv::DIV8),
+            divr: Some(PllRDiv::DIV2),
+        });
+
+        rcc.sys = Sysclk::PLL1_R;
+        rcc.ahb_pre = AHBPrescaler::DIV1;
+        rcc.apb1_pre = APBPrescaler::DIV1;
+        rcc.apb2_pre = APBPrescaler::DIV1;
+
+        rcc.ls = LsConfig::default_lse();
+
+        rcc.mux.rtcsel = RtcClockSource::LSE;
+        rcc.mux.adc12sel = Adcsel::SYS;
+        rcc.mux.adc345sel = Adcsel::SYS;
+        rcc.mux.clk48sel = Clk48sel::HSI48;
+        rcc.mux.fdcansel = Fdcansel::PLL1_Q;
+
+        config
+    };
+    let p = embassy_stm32::init(config);
+
     let mut status_led = Output::new(p.PB14, Level::Low, Speed::Low);
     let mut led1 = Output::new(p.PC13, Level::Low, Speed::Low);
     let mut led2 = Output::new(p.PA10, Level::Low, Speed::Low);
