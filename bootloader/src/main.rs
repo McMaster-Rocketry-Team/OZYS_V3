b1ce4f0e19d9489c52d3ad940988aae60443d91f
Pegasis <me@pegasis.site>
2025-05-16 12:03:50 -0400

    new bootloader

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -1,87 +1,193 @@
 #![no_std]
 #![no_main]
+#![feature(let_chains)]
 
-use core::cell::RefCell;
+mod can;
+mod ota;
 
-use cortex_m::asm;
-use cortex_m_rt::{entry, exception};
-// use defmt::{info, warn};
-use embassy_boot_stm32::*;
+use core::mem::MaybeUninit;
+
+use can::start_can_bus_tasks;
+use cortex_m::singleton;
+use cortex_m_rt::entry;
+use defmt::*;
+use embassy_executor::Executor;
 use embassy_stm32::{
-    flash::{Flash, BANK1_REGION},
     gpio::{Level, Output, Speed},
+    peripherals::PB14,
+    time::mhz,
+    wdg::IndependentWatchdog,
+    Peri,
 };
-use embassy_sync::blocking_mutex::Mutex;
-// use {defmt_rtt as _, panic_probe as _};
+use embassy_sync::blocking_mutex::raw::NoopRawMutex;
+use embassy_time::{Duration, Instant, Ticker, Timer};
+use firmware_common_new::can_bus::{
+    messages::{
+        node_status::{NodeHealth, NodeMode},
+        NodeStatusMessage,
+    },
+    sender::CanSender,
+};
+use {defmt_rtt as _, panic_probe as _};
+
+/// # BACKUP_RAM\[0\]:
+///
+/// 0x69426942 to indicate the next boot should go into bootloader.
+///
+/// # Trial boots
+///
+/// Prior to loading the main application, the bootloader will keep BACKUP_RAM\[0\]
+/// at 0x69426942 and start a watchdog that if not refreshed in 1 second, will reset
+/// the device.
+///
+/// After the main application is started, it should reset BACKUP_RAM\[0\] to 0 and
+/// refresh or disable the watchdog.
+///
+/// If the main application failed to start, the watchdog will reset the device and
+/// due to the magic number in BACKUP_RAM\[0\], the device will stay in bootloader.
+#[link_section = ".backup_ram"]
+static mut BACKUP_RAM: MaybeUninit<[u32; 256]> = MaybeUninit::uninit();
+
+pub enum BootOption {
+    Bootloader,
+    Application,
+}
+
+pub fn configure_next_boot(boot_option: BootOption) {
+    let backup_ram = unsafe {
+        #[allow(static_mut_refs)]
+        BACKUP_RAM.assume_init_mut()
+    };
+    match boot_option {
+        BootOption::Bootloader => {
+            backup_ram[0] = 0x69426942;
+        }
+        BootOption::Application => {
+            backup_ram[0] = 0;
+        }
+    }
+}
+
+// this needs to be the same as the APP section origin in memory.x
+pub const APP_ADDRESS: u32 = 0x08010000;
 
 #[entry]
 fn main() -> ! {
-    let p = embassy_stm32::init(Default::default());
-    let mut bl_led1 = Output::new(p.PC13, Level::High, Speed::Low);
-    let mut first_led4 = Output::new(p.PB7, Level::Low, Speed::Low);
-    let mut updated_led3 = Output::new(p.PB6, Level::Low, Speed::Low);
-    for i in 0..5000000 {
-        asm::nop();
-    }
-    // info!("Hello bootloader!");
+    let config = {
+        use embassy_stm32::rcc::mux::*;
+        use embassy_stm32::rcc::*;
+        let mut config = embassy_stm32::Config::default();
+        let rcc = &mut config.rcc;
 
-    // Uncomment this if you are debugging the bootloader with debugger/RTT attached,
-    // as it prevents a hard fault when accessing flash 'too early' after boot.
-    // for i in 0..1000000 {
-    //     asm::nop();
-    // }
+        rcc.hsi = false;
+        rcc.hse = Some(Hse {
+            freq: mhz(16),
+            mode: HseMode::Oscillator,
+        });
+        rcc.hsi48 = Some(Hsi48Config {
+            sync_from_usb: true,
+        });
 
-    let layout = Flash::new_blocking(p.FLASH).into_blocking_regions();
-    let flash_bank1 = Mutex::new(RefCell::new(layout.bank1_region));
-    let flash_bank2 = Mutex::new(RefCell::new(layout.bank2_region));
+        // Limit clock speed to 150Mhz to reduce power consumption
+        rcc.pll = Some(Pll {
+            source: PllSource::HSE,
+            prediv: PllPreDiv::DIV4,
+            mul: PllMul::MUL75,
+            divp: Some(PllPDiv::DIV2),
+            divq: Some(PllQDiv::DIV2),
+            divr: Some(PllRDiv::DIV2),
+        });
 
-    let config =
-        BootLoaderConfig::from_linkerfile_blocking(&flash_bank1, &flash_bank2, &flash_bank1);
-    let active_offset = config.active.offset();
+        rcc.sys = Sysclk::PLL1_R;
+        rcc.ahb_pre = AHBPrescaler::DIV1;
+        rcc.apb1_pre = APBPrescaler::DIV1;
+        rcc.apb2_pre = APBPrescaler::DIV1;
 
-    // unsafe {
-    //     let mut p = cortex_m::Peripherals::steal();
-    //     p.SCB.invalidate_icache();
-    //     p.SCB.vtor.write(BANK1_REGION.base + active_offset);
+        rcc.ls = LsConfig::default_lse();
 
-    //     cortex_m::asm::bootload((BANK1_REGION.base + active_offset) as *const u32);
-    // }
+        rcc.mux.rtcsel = RtcClockSource::LSE;
+        rcc.mux.adc12sel = Adcsel::SYS;
+        rcc.mux.adc345sel = Adcsel::SYS;
+        rcc.mux.clk48sel = Clk48sel::HSI48;
+        rcc.mux.fdcansel = Fdcansel::PLL1_Q;
 
-    let result = BootLoader::try_prepare::<_, _, _, 2048>(config);
+        config
+    };
+    let p = embassy_stm32::init(config);
 
-    match result {
-        Ok(bl) => {
-            // info!(
-            //     "bootloader initialized, booting to address {:X}",
-            //     BANK1_REGION.base + active_offset
-            // );
-            unsafe { bl.load(BANK1_REGION.base + active_offset) }
-        }
-        Err(e) => {
-            // warn!("failed to perpare bootloader: {}", e);
-        }
+    let backup_ram = unsafe {
+        #[allow(static_mut_refs)]
+        BACKUP_RAM.assume_init_mut()
     };
 
+    if backup_ram[0] != 0x69426942 {
+        // no magic number found, boot normally
+        let mut wdt = IndependentWatchdog::new(p.IWDG, 1_000_000);
+        wdt.unleash();
+
+        unsafe {
+            let mut p = cortex_m::Peripherals::steal();
+            p.SCB.invalidate_icache();
+            p.SCB.vtor.write(APP_ADDRESS);
+            cortex_m::asm::bootload(APP_ADDRESS as *const u32);
+        }
+    }
+    // enter bootloader only if magic number detected
+
+    // this is ordered by led from top to bottom, power connector on the top left
+    // LED order from top to bottom: (power connector on the top left)
+    // led 2: PA10
+    // led 3: PB6
+    // led 4: PB7
+    // led 1: PC13
+    // status led: PB14
+    let mut _led2 = Output::new(p.PA10, Level::Low, Speed::Low);
+    let mut _led3 = Output::new(p.PB6, Level::Low, Speed::Low);
+    let mut _led4 = Output::new(p.PB7, Level::Low, Speed::Low);
+    let mut _led1 = Output::new(p.PC13, Level::Low, Speed::Low);
+
+    info!("Bootloader!");
+    let executor = singleton!(: Executor = Executor::new()).unwrap();
+    executor.run(|spawner| {
+        spawner.must_spawn(status_led_task(p.PB14));
+        let (self_can_node_id, can_sender, can_receiver) =
+            start_can_bus_tasks(&spawner, p.FDCAN3, p.PA8, p.PA15);
+        spawner.must_spawn(node_status_task(can_sender));
+    });
+}
+
+#[embassy_executor::task]
+async fn status_led_task(yellow_led: Peri<'static, PB14>) {
+    let mut yellow_led = Output::new(yellow_led, Level::High, Speed::Low);
+
+    let mut ticker = Ticker::every(Duration::from_millis(500));
     loop {
-        asm::nop();
+        yellow_led.set_high();
+        Timer::after_millis(50).await;
+        yellow_led.set_low();
+        Timer::after_millis(50).await;
+        yellow_led.set_high();
+        Timer::after_millis(50).await;
+        yellow_led.set_low();
+        ticker.next().await;
     }
 }
 
-#[no_mangle]
-#[cfg_attr(target_os = "none", link_section = ".HardFault.user")]
-unsafe extern "C" fn HardFault() {
-    cortex_m::peripheral::SCB::sys_reset();
-}
-
-#[exception]
-unsafe fn DefaultHandler(_: i16) -> ! {
-    const SCB_ICSR: *const u32 = 0xE000_ED04 as *const u32;
-    let irqn = core::ptr::read_volatile(SCB_ICSR) as u8 as i16 - 16;
-
-    panic!("DefaultHandler #{:?}", irqn);
-}
-
-#[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
-    cortex_m::asm::udf();
+#[embassy_executor::task]
+async fn node_status_task(can_sender: &'static CanSender<NoopRawMutex, 4>) {
+    let mut ticker = Ticker::every(Duration::from_millis(500));
+    loop {
+        can_sender
+            .send(
+                NodeStatusMessage {
+                    uptime_s: Instant::now().as_secs() as u32,
+                    health: NodeHealth::Healthy,
+                    mode: NodeMode::Maintainance,
+                    custom_status: 0,
+                }
+                .into(),
+            )
+            .await;
+        ticker.next().await;
+    }
 }
---
+++ bootloader/src/ota.rs
@@ -0,0 +1,157 @@
+use defmt::*;
+use embassy_futures::select::{select, Either};
+use embassy_stm32::{
+    flash::{Flash, WRITE_SIZE},
+    peripherals::FLASH,
+    Peri,
+};
+use embassy_sync::{
+    blocking_mutex::raw::{NoopRawMutex, RawMutex},
+    pipe::{Reader, Writer},
+    signal::Signal,
+};
+use embassy_time::Timer;
+use embedded_io_async::{Read, Write};
+use firmware_common_new::{
+    can_bus::{
+        messages::{ack::AckMessage, data_transfer::DataType, CanBusMessageEnum},
+        receiver::CanReceiver,
+        sender::CanSender,
+    },
+    signal_with_ack::{SignalWithAckReceiver, SignalWithAckSender},
+};
+
+use crate::{configure_next_boot, BootOption, APP_ADDRESS};
+
+#[embassy_executor::task]
+async fn ota_write_disk_task(
+    flash: Peri<'static, FLASH>,
+    mut pipe_rx: Reader<'static, NoopRawMutex, 64>,
+    reset_signal_receiver: SignalWithAckReceiver<'static, NoopRawMutex, (), ()>,
+    receive_done_signal: &'static Signal<NoopRawMutex, ()>,
+) {
+    let mut flash = Flash::new_blocking(flash);
+    const PAGE_SIZE: u32 = 2 * 1024;
+    let mut firmware_write_offset = APP_ADDRESS - 0x08000000;
+    let mut pipe_rx_read_buffer = [0u8; WRITE_SIZE];
+
+    loop {
+        let reset_signal_fut = reset_signal_receiver.wait();
+        let read_pipe_fut = pipe_rx.read_exact(&mut pipe_rx_read_buffer);
+        match select(reset_signal_fut, read_pipe_fut).await {
+            Either::First(_) => {
+                // firmware transmission is going to start
+
+                // empty the existing data in the pipe
+                loop {
+                    if pipe_rx.try_read(&mut pipe_rx_read_buffer).is_err() {
+                        break;
+                    }
+                }
+                // reset write offset
+                firmware_write_offset = APP_ADDRESS - 0x08000000;
+
+                reset_signal_receiver.ack(());
+                receive_done_signal.reset();
+            }
+            Either::Second(_) => {
+                // Pipe can't EOF, so we dont need to handle the result returned by read_exact
+                if firmware_write_offset % PAGE_SIZE == 0 {
+                    flash
+                        .blocking_erase(firmware_write_offset, firmware_write_offset + PAGE_SIZE)
+                        .unwrap();
+                }
+                flash
+                    .blocking_write(firmware_write_offset, &pipe_rx_read_buffer)
+                    .unwrap();
+                firmware_write_offset += WRITE_SIZE as u32;
+            }
+        };
+
+        if receive_done_signal.try_take().is_some() {
+            // all firmware has been received in the pipe
+            loop {
+                if pipe_rx.try_read_exact(&mut pipe_rx_read_buffer).is_err() {
+                    break;
+                }
+                if firmware_write_offset % PAGE_SIZE == 0 {
+                    flash
+                        .blocking_erase(firmware_write_offset, firmware_write_offset + PAGE_SIZE)
+                        .unwrap();
+                }
+                flash
+                    .blocking_write(firmware_write_offset, &pipe_rx_read_buffer)
+                    .unwrap();
+                // TODO verify firmware is signed
+
+                configure_next_boot(BootOption::Application);
+                cortex_m::peripheral::SCB::sys_reset();
+            }
+        }
+    }
+}
+
+#[embassy_executor::task]
+async fn ota_receive_task(
+    self_can_node_id: u16,
+    can_sender: &'static CanSender<NoopRawMutex, 4>,
+    can_receiver: &'static CanReceiver<NoopRawMutex, 4, 1>,
+    mut pipe_tx: Writer<'static, NoopRawMutex, 64>,
+    restart_signal_sender: SignalWithAckSender<'static, NoopRawMutex, (), ()>,
+    receive_done_signal: &'static Signal<NoopRawMutex, ()>,
+) {
+    let mut can_sub = can_receiver.subscriber().unwrap();
+    loop {
+        let message = can_sub.next_message_pure().await.data;
+
+        if let CanBusMessageEnum::DataTransfer(data_transfer) = message.message
+            && data_transfer.data_type == DataType::Firmware
+            && data_transfer.destination_node_id == self_can_node_id
+        {
+            if data_transfer.start_of_transfer {
+                info!("Starting to receive firmware.....");
+                restart_signal_sender.send_and_wait_for_ack(()).await;
+            }
+
+            pipe_tx.write_all(data_transfer.data()).await.unwrap();
+
+            can_sender
+                .send(
+                    AckMessage {
+                        crc: message.crc,
+                        node_id: message.id.node_id,
+                    }
+                    .into(),
+                )
+                .await;
+
+            if data_transfer.end_of_transfer {
+                info!("Received firmware successfully");
+                can_sender.flush().await;
+                Timer::after_millis(50).await;
+                receive_done_signal.signal(());
+            }
+        }
+    }
+}
+
+trait TryReadExact {
+    fn try_read_exact(&mut self, buf: &mut [u8]) -> Result<usize, ()>;
+}
+
+impl<'p, M: RawMutex, const N: usize> TryReadExact for Reader<'p, M, N> {
+    fn try_read_exact(&mut self, mut buf: &mut [u8]) -> Result<usize, ()> {
+        let mut total_read = 0;
+        while !buf.is_empty() {
+            match self.try_read(buf) {
+                Ok(0) => break,
+                Ok(n) => {
+                    total_read += n;
+                    buf = &mut buf[n..];
+                }
+                Err(_) => return Err(()),
+            }
+        }
+        Ok(total_read)
+    }
+}
